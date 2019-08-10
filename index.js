f77d1898436267263bcdd5f1cabd04277d6857bf
Vanessa Corlette <nessacor16@gmail.com>
2019-08-10 20:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 function add(a, b) {
-  return a + b,
+  return a + b
 }
 function subtract(a, b) {
-  return a - b,
+  return a - b
 }
 function multiply(a, b) {
-  return a * b,
+  return a * b
 }
-function divide(ab) {
-  return a / b,
+function divide(a, b) {
+  return a / b
 }
-function (inc(n)) {
-  var x = 15
-  return 15
-}
-function (dec(n)) {
-  var x = 30
-  return 30
-}
-"30"*15
-  return 450
